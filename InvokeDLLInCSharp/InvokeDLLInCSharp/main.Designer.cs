d700a19c8e2a3534bb80dfa26aad833e164bbfee
Boston Hsu <bostonhsu@outlook.com>
2017-07-18 09:55:40 +0800

    next

--- InvokeDLLInCSharp/InvokeDLLInCSharp/main.Designer.cs
+++ InvokeDLLInCSharp/InvokeDLLInCSharp/main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace InvokeDLLInCSharp
 {
-    partial class Form1
+    partial class main
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -28,20 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnDoTest = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // btnDoTest
+            // 
+            this.btnDoTest.Location = new System.Drawing.Point(69, 73);
+            this.btnDoTest.Name = "btnDoTest";
+            this.btnDoTest.Size = new System.Drawing.Size(146, 43);
+            this.btnDoTest.TabIndex = 0;
+            this.btnDoTest.Text = "DoTest";
+            this.btnDoTest.UseVisualStyleBackColor = true;
+            // 
+            // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(445, 352);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.btnDoTest);
+            this.Name = "main";
+            this.Text = "MainForm";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnDoTest;
     }
 }
 
