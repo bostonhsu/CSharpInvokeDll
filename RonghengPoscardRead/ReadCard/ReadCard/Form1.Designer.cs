4e6358edeec9ea4a9333f267c8d7ca46e03a3f2c
Boston Hsu <bostonhsu@outlook.com>
2017-07-19 10:45:12 +0800

    为精铸公司培训读卡器做好了程序修改。

--- RonghengPoscardRead/ReadCard/ReadCard/Form1.Designer.cs
+++ RonghengPoscardRead/ReadCard/ReadCard/Form1.Designer.cs
@@ -33,14 +33,19 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMenjin = new System.Windows.Forms.TextBox();
+            this.btnRecard = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(707, 38);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(518, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 29);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "打开串口";
             this.button1.UseVisualStyleBackColor = true;
@@ -48,10 +53,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(707, 141);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(543, 146);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 29);
+            this.button2.Size = new System.Drawing.Size(98, 35);
             this.button2.TabIndex = 1;
             this.button2.Text = "读卡";
             this.button2.UseVisualStyleBackColor = true;
@@ -59,21 +64,19 @@
             // 
             // listBox1
             // 
-            this.listBox1.Font = new System.Drawing.Font("SimSun", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox1.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 47;
-            this.listBox1.Location = new System.Drawing.Point(11, 11);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(8, 9);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(670, 239);
+            this.listBox1.Size = new System.Drawing.Size(504, 172);
             this.listBox1.TabIndex = 2;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(707, 193);
-            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Location = new System.Drawing.Point(652, 72);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(100, 29);
+            this.button4.Size = new System.Drawing.Size(47, 23);
             this.button4.TabIndex = 4;
             this.button4.Text = "断开";
             this.button4.UseVisualStyleBackColor = true;
@@ -81,29 +84,93 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(803, 85);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Location = new System.Drawing.Point(624, -3);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 29);
+            this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 5;
             this.button3.Text = "鸣叫";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtName.Location = new System.Drawing.Point(81, 220);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(153, 35);
+            this.txtName.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(13, 227);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "姓名：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(245, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "门禁号：";
+            // 
+            // txtMenjin
+            // 
+            this.txtMenjin.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtMenjin.Location = new System.Drawing.Point(336, 220);
+            this.txtMenjin.Name = "txtMenjin";
+            this.txtMenjin.Size = new System.Drawing.Size(138, 35);
+            this.txtMenjin.TabIndex = 9;
+            // 
+            // btnRecard
+            // 
+            this.btnRecard.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnRecard.Location = new System.Drawing.Point(508, 218);
+            this.btnRecard.Name = "btnRecard";
+            this.btnRecard.Size = new System.Drawing.Size(98, 35);
+            this.btnRecard.TabIndex = 10;
+            this.btnRecard.Text = "记录";
+            this.btnRecard.UseVisualStyleBackColor = true;
+            this.btnRecard.Click += new System.EventHandler(this.btnRecard_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(545, 297);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(60, 24);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(934, 423);
+            this.ClientSize = new System.Drawing.Size(700, 338);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btnRecard);
+            this.Controls.Add(this.txtMenjin);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -114,6 +181,12 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMenjin;
+        private System.Windows.Forms.Button btnRecard;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- RonghengPoscardRead/ReadCard/ReadCard/Form1.cs
+++ RonghengPoscardRead/ReadCard/ReadCard/Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 
@@ -6,6 +8,9 @@ namespace ReadCard
 {
     public partial class Form1 : Form
     {
+        private bool isCorrectEmployee;
+        private string scardTemp;
+
         public int icdev; // 通讯设备标识符
         public Int16 st;
         [DllImport("mwrf32.dll", EntryPoint = "rf_init", SetLastError = true,
@@ -32,17 +37,85 @@ namespace ReadCard
         //说明：     返回设备当前状态
         public static extern Int16 rf_card(int icdev, int bcnt, out uint snr);
 
+        #region Database Connection
+        //数据连接最基本需要的两个对象
+        private SqlConnection conn = null;
+        private SqlCommand cmd = null;
+        //private SqlDataAdapter adapter = null;
+        //为了方便，设为全局对象的sql语句
+        private string sql = null;
+        //公用 打开数据库的方法
+        private const string ConnString = "Data Source=192.168.20.3;Initial Catalog=PostekDB;UID=Bostonhsu;Password=sa";
+
+        public void openDatabase()
+        {
+            conn = new SqlConnection();
+            conn.ConnectionString = ConnString;
+
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+                //Response.Write("<script>alert('Connected!');</script>");
+            }
+        }
+        //默认加载页面的方法 找到年龄最大的加载
+        //有些问题，年龄不能相同，加载中前台的textbox里只能显示一条记录，数据拿到之后有多条只显示一条
+        public void load()
+        {
+            //openDatabase();
+            //cmd = new SqlCommand("select * from users where age=(select max(age) from users)", conn);
+            //SqlDataReader dr = cmd.ExecuteReader();
+            //if (dr.Read())
+            //{
+            //    tbName.Text = (String)dr[1].ToString().Trim();
+            //    tbAge.Text = (String)dr[2].ToString().Trim();
+            //}
+            //conn.Close();
+        }
+        //根据sql语句加载信息，重载两个textbox
+        public void load(String sql)
+        {
+            //openDatabase();
+            //cmd = new SqlCommand(sql, conn);
+            //SqlDataReader dr = cmd.ExecuteReader();
+            //while (dr.Read())
+            //{
+            //    tbName.Text = (String)dr[1].ToString().Trim();
+            //    tbAge.Text = (String)dr[2].ToString().Trim();
+            //}
+            //conn.Close();
+        }
+        //封装的数据库语句执行的方法
+        public void execute(String sql)
+        {
+            openDatabase();
+            cmd = new SqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+#endregion
+
         public Form1()
         {
             InitializeComponent();
+            isCorrectEmployee = false;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button4.Enabled = false;
+            btnRecard.Enabled = false;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)  // open serial port
         {
             icdev = rf_init(2, 115200);
             if (icdev > 0)
             {
                 listBox1.Items.Add("打开成功:" + Convert.ToString(icdev));
+                rf_beep(icdev, 3);
+                button1.Enabled = false;
+                button2.Enabled = true;
+                button4.Enabled = true;
+                btnRecard.Enabled = true;
             }
             else
             {
@@ -50,7 +123,7 @@ namespace ReadCard
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)      // Read Poscard
         {
             uint snr = 0;
             st = rf_card(icdev, 0, out snr);
@@ -61,17 +134,111 @@ namespace ReadCard
             }
             string scardcode = snr.ToString();
             scardcode = scardcode.PadLeft(10, '0');
-            listBox1.Items.Add("读卡成功:" + scardcode);            
+            scardTemp = scardcode;
+            listBox1.Items.Add("读卡成功:" + scardcode);
+            confirmEmployee(scardcode);
+            listBox1.TopIndex = listBox1.Items.Count - (int)(listBox1.Height / listBox1.ItemHeight);
+            rf_beep(icdev, 15);
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void confirmEmployee(string scardcode)
+        {
+            // 把种类描述列在这里。
+            //初始化一个数据集 
+            DataSet DataTemp = new DataSet();
+            //初始化一个连接集 
+            SqlConnection _objCon = new SqlConnection(ConnString);
+            //打开连接 
+            _objCon.Open();
+            //做一个连接器 
+            SqlDataAdapter _myAdapter;
+            //scardcode = "1210875902";
+            //scardTemp = scardcode;
+            string queryStr = "SELECT BS_ACCO_INFO.ACCO_STUD_CODE, BS_ACCO_INFO.ACCO_NAME FROM BS_ACCO_INFO inner join BS_ACCO_CARD ON BS_ACCO_INFO.ACCO_ID = BS_ACCO_CARD.CARD_ACCO_ID WHERE(CARD_CODE = '" + scardcode + "')";
+            _myAdapter = new SqlDataAdapter(queryStr, _objCon);
+            //创建增删改查command对象 
+            SqlCommandBuilder sqlCommand = new SqlCommandBuilder(_myAdapter);
+            //将数据库中的数据填充到数据集的表中,设置表名 
+            _myAdapter.Fill(DataTemp, "temp");
+
+            try
+            {
+                if (DataTemp.Tables[0].Rows.Count > 0)
+                {
+                    isCorrectEmployee = true;
+                    txtName.Text = DataTemp.Tables[0].Rows[0][1].ToString();
+                    txtMenjin.Text = DataTemp.Tables[0].Rows[0][0].ToString();
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            sqlCommand.Dispose();
+            _myAdapter.Dispose();
+            _objCon.Close();
+            _objCon.Dispose();
+        }
+
+        private void button4_Click(object sender, EventArgs e)      // Close serial port
         {
             rf_exit(icdev);
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button4.Enabled = false;
+            btnRecard.Enabled = false;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             rf_beep(icdev, 10);
+        }
+
+        private void btnRecard_Click(object sender, EventArgs e)
+        {
+            if (isCorrectEmployee)
+            {
+                saveEmployeeToDB();
+                isCorrectEmployee = false;
+                MessageBox.Show("添加员工：" + txtName.Text.Trim() + "， 成功。");
+                clearTextBoxContent();
+            }
+        }
+
+        private void clearTextBoxContent()
+        {
+            txtName.Text = "";
+            txtMenjin.Text = "";
+        }
+
+        private void saveEmployeeToDB()
+        {
+            //初始化一个数据集 
+            DataSet DataTemp = new DataSet();
+            //初始化一个连接集 
+            SqlConnection _objCon = new SqlConnection(ConnString);
+            //打开连接 
+            _objCon.Open();
+            //做一个连接器 
+            SqlDataAdapter _myAdapter;
+            string temp = "insert into Temp_BostonHsu (Name, ScardCode, MenjinCode) values ('" + txtName.Text.Trim() + "', '" + scardTemp + "', '" + txtMenjin.Text.Trim() + "')";
+            _myAdapter = new SqlDataAdapter(temp, _objCon);
+            //创建增删改查command对象 
+            SqlCommandBuilder sqlCommand = new SqlCommandBuilder(_myAdapter);
+            //将数据库中的数据填充到数据集的表中,设置表名 
+            _myAdapter.Fill(DataTemp, "temp");
+
+            sqlCommand.Dispose();
+            _myAdapter.Dispose();
+            _objCon.Close();
+            _objCon.Dispose();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            isCorrectEmployee = true;
+            confirmEmployee("");
+            btnRecard_Click(sender, e);
         }
     }
 }
